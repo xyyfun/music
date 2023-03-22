4b04fbe199f9cd0689149bd07cc1631285f39587
Turmp <1737293634@qq.com>
2023-03-23 00:12:41 +0800

    配置根组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
+	<router-view></router-view>
 </template>
-
-<script>
-export default {
-	name: 'App',
-};
-</script>
-
-<style></style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="container overflow">
+		<AppSidebar />
+		<div class="main-container">
+			<AppHeader />
+			<router-view></router-view>
+			<AppProgress />
+		</div>
+	</div>
+</template>
+
+<script>
+import AppSidebar from '@/components/app-sidebar';
+import AppHeader from '@/components/app-header';
+import AppProgress from '@/components/app-progress';
+export default {
+	name: 'Layout',
+	components: { AppSidebar, AppHeader, AppProgress },
+};
+</script>
+
+<style lang="less">
+.container {
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+	.main-container {
+		flex: 1;
+		display: flex;
+		justify-content: space-between;
+		flex-direction: column;
+	}
+}
+</style>
