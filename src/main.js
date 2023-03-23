54ccc7c34062e4369e455490c60317af42c31f83
Turmp <1737293634@qq.com>
2023-03-23 23:18:59 +0800

    引入样式

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from '@/router';
+import './assets/style/common.less';
+import './assets/iconfont/iconfont.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
