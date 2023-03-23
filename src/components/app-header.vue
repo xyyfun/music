e03aad6ef9940f3c088d3eac86456b7923b7793b
Turmp <1737293634@qq.com>
2023-03-23 23:19:53 +0800

    头部组件基础样式完成

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,5 +1,27 @@
 <template>
-	<div class="app-header"></div>
+	<div class="app-header">
+		<div class="header-content">
+			<!-- 工具 -->
+			<div class="tool">
+				<!-- 历史/前进/后退 -->
+				<div class="history">
+					<a href="javascript:;" title="后退"><i class="iconfont icon-xiangzuojiantou"></i></a>
+					<a href="javascript:;" title="前进"><i class="iconfont icon-xiangyoujiantou"></i></a>
+				</div>
+				<!-- 搜索 -->
+				<div class="search">
+					<input type="text" />
+				</div>
+			</div>
+			<!-- 菜单 -->
+			<div class="menu">
+				<div class="picture"></div>
+				<a href="javascript:;" class="info">请登录</a>
+				<a href="javascript:;" class="skin"><i class="iconfont icon-pifu"></i></a>
+				<a href="javascript:;" class="more"><i class="iconfont icon-caidan"></i></a>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -8,10 +30,56 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .app-header {
 	width: 100%;
-	height: 60px;
-	background-color: #000;
+	height: 4.2rem;
+	.header-content {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: 100%;
+		padding: 0 1.78rem;
+		> div {
+			height: 2rem;
+			line-height: 2rem;
+		}
+		.tool {
+			display: flex;
+			.history {
+				i {
+					padding: 0 0.7rem;
+					font-size: 1rem;
+				}
+			}
+			.search {
+				> input {
+					outline: none;
+					height: 2rem;
+					width: 20rem;
+					border: none;
+					border-radius: 1rem;
+					background-color: #e3e3e3;
+					text-indent: 0.7rem;
+				}
+			}
+		}
+		.menu {
+			display: flex;
+			.picture {
+				width: 2rem;
+				height: 2rem;
+				background-color: #c0c4cc;
+				border-radius: 50%;
+			}
+			> a {
+				margin: 0 0.5rem;
+				font-size: 1rem;
+				i {
+					font-size: 1rem;
+				}
+			}
+		}
+	}
 }
 </style>
