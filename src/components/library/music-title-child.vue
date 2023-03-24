fd330b1dd01bd900988f38dda13183b683008fe8
Turmp <1737293634@qq.com>
2023-03-24 23:56:42 +0800

    子标题组件封装

---
+++ src/components/library/music-title-child.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="title">{{ title }}<i class="iconfont icon-xiangyoujiantou"></i></div>
+</template>
+
+<script>
+export default {
+	name: 'MusicTitle',
+	props: ['title'],
+};
+</script>
+
+<style lang="less" scoped>
+.title {
+	display: flex;
+	align-items: center;
+	padding: 1rem 0;
+	font-size: 1.42rem;
+	i {
+		font-size: 1rem;
+	}
+}
+</style>
