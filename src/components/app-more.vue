d3da9e413bdc8401ade99eae186134f14d8319e7
Turmp <1737293634@qq.com>
2023-04-27 21:14:59 +0800

    优化“加载更多”加载方式

--- src/components/app-more.vue
+++ src/components/app-more.vue
@@ -1,25 +1,38 @@
 <template>
-	<div class="more">
-		<a v-if="isMore" class="yes" href="javascript:;" @click="loadMore">加载更多</a>
+	<div class="more" ref="target">
+		<a v-if="isMore" class="yes" href="javascript:;">努力加载中...</a>
 		<a v-else class="not" href="javascript:;">没有更多了...</a>
 	</div>
 </template>
 
 <script>
+import { ref, watch } from 'vue';
+import { useIntersectionObserver } from '@vueuse/core';
 export default {
 	name: 'AppMore',
 	props: {
 		isMore: {
 			type: Boolean,
-			default: true,
+			default: false,
 		},
 	},
 	emits: ['loadMore'],
 	setup(props, { emit }) {
-		const loadMore = () => {
-			emit('loadMore');
-		};
-		return { loadMore };
+		const target = ref(null);
+		const targetIsVisible = ref(false);
+		const { stop } = useIntersectionObserver(target, ([{ isIntersecting }], observerElement) => {
+			targetIsVisible.value = isIntersecting;
+		});
+		watch(targetIsVisible, newVal => {
+			if (newVal === false) {
+				console.log('离开可视区域！');
+			} else {
+				console.log('进入可视区域！');
+			}
+			// 出现可视区且拥有更多数据时请求数据
+			if (props.isMore && newVal) emit('loadMore');
+		});
+		return { target };
 	},
 };
 </script>
