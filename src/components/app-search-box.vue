ae8a78fc67d11fce3d4548f141b09377ae650921
Turmp <1737293634@qq.com>
2023-05-09 23:54:20 +0800

    新增历史搜索&优化热搜显示

--- src/components/app-search-box.vue
+++ src/components/app-search-box.vue
@@ -11,11 +11,11 @@
 				v-model="keywords" />
 		</div>
 		<div class="searchList" v-if="isShowList" ref="target">
-			<div class="songs" v-if="suggest.songs">
+			<div class="suggest-songs" v-if="suggest.songs">
 				<span>单曲</span>
 				<ul>
-					<li v-for="item in suggest.songs" :key="item.id" @click="changKeywords(item.name)">
-						<router-link :to="`/search/${item.name}`">
+					<li v-for="item in suggest.songs" :key="item.id">
+						<router-link :to="`/search/${item.name}`" @click="changKeywords(item.name)">
 							{{ item.name }} -
 							<router-link
 								class="singer"
@@ -27,50 +27,79 @@
 					</li>
 				</ul>
 			</div>
-			<div class="playlists" v-if="suggest.playlists">
+			<div class="suggest-playlists" v-if="suggest.playlists">
 				<span>歌单</span>
 				<ul>
-					<li v-for="item in suggest.playlists" :key="item.id" @click="changKeywords(item.name)">
-						<router-link :to="`/playlist/${item.id}`">{{ item.name }}</router-link>
+					<li v-for="item in suggest.playlists" :key="item.id">
+						<router-link :to="`/playlist/${item.id}`" @click="changKeywords(item.name)">{{
+							item.name
+						}}</router-link>
 					</li>
 				</ul>
 			</div>
-			<div class="artists" v-if="suggest.artists">
+			<div class="suggest-artists" v-if="suggest.artists">
 				<span>歌手</span>
 				<ul>
-					<li v-for="item in suggest.artists" :key="item.id" @click="changKeywords(item.name)">
-						<router-link :to="`/singer/${item.id}`">
+					<li v-for="item in suggest.artists" :key="item.id">
+						<router-link :to="`/singer/${item.id}`" @click="changKeywords(item.name)">
 							<img :src="item.picUrl + '?param=130y130'" alt="" />{{ item.name }}
 						</router-link>
 					</li>
 				</ul>
 			</div>
-			<div class="albums" v-if="suggest.albums">
+			<div class="suggest-albums" v-if="suggest.albums">
 				<span>专辑</span>
 				<ul>
-					<li v-for="item in suggest.albums" :key="item.id" @click="changKeywords(item.name)">
-						<router-link :to="`/album/${item.id}`"
-							><img :src="item.artist.picUrl + '?param=130y130'" alt="" />{{
-								item.name
-							}}</router-link
-						>
-					</li>
-				</ul>
-			</div>
-			<div class="search-hot" v-if="searchHotList.length">
-				<ul>
-					<span>热门搜索</span>
-					<li
-						v-for="(item, index) in searchHotList"
-						:key="index"
-						@click="changKeywords(item.searchWord)">
-						<router-link :to="`/search/${item.searchWord}`">
-							<span>{{ index + 1 }}</span>
-							<span class="ellipsis">{{ item.searchWord }}</span>
-							<span class="score">{{ item.score }}</span>
+					<li v-for="item in suggest.albums" :key="item.id">
+						<router-link :to="`/album/${item.id}`" @click="changKeywords(item.name)">
+							<img :src="item.artist.picUrl + '?param=130y130'" alt="" />
+							{{ item.name }}
 						</router-link>
 					</li>
 				</ul>
+			</div>
+			<div class="search-prompt scroll" v-if="searchHotList.length">
+				<div class="history">
+					<div class="head-history">
+						<span>搜索历史</span>
+						<a href="javascript:;" @click="removeHistorySearch(true)" title="删除历史记录">
+							<i class="iconfont icon-remove"></i>
+						</a>
+					</div>
+					<ul>
+						<li v-for="item in HistorySearch" :key="item.id">
+							<router-link :to="`/search/${item.keyword}`" @click="changKeywords(item.keyword)">
+								{{ item.keyword }}
+							</router-link>
+							<a href="javascript:;" @click="removeHistorySearch(false, item.id)">
+								<i class="iconfont icon-cuo"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="hot">
+					<ul>
+						<span>热门搜索</span>
+						<li
+							v-for="(item, index) in searchHotList"
+							:key="index"
+							@click="changKeywords(item.searchWord)">
+							<router-link :to="`/search/${item.searchWord}`">
+								<div class="sort">
+									<span>{{ index + 1 }}</span>
+								</div>
+								<div class="info">
+									<div class="msg ellipsis">
+										<span v-if="item.searchWord" class="searchWord">{{ item.searchWord }}</span>
+										<img v-if="item.iconUrl" :src="item.iconUrl" alt="" />
+										<span>{{ item.score }}</span>
+									</div>
+									<span class="content" v-if="item.content">{{ item.content }}</span>
+								</div>
+							</router-link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -79,10 +108,11 @@
 <script>
 import { onClickOutside } from '@vueuse/core';
 import { defaultKeyword, searchSuggest, searchHot } from '@/api/search';
-import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import useNumberSwitch from '@/hooks/useNumberSwitch';
 import debounce from 'lodash/debounce';
+import { getHistorySearch, setHistorySearch } from '@/utils/history';
 export default {
 	name: 'AppSearchBox',
 	setup() {
@@ -93,16 +123,42 @@ export default {
 		const suggest = ref({});
 		const searchHotList = ref([]);
 		const target = ref(null);
-		// 搜索
+		const HistorySearch = ref([]);
+		// 添加历史搜索
+		const addNewHistorySearch = keyword => {
+			const history = getHistorySearch();
+			const result = history.some(e => e.keyword === keyword);
+			if (result) return;
+			history.unshift({ keyword, id: Date.now() }); // 添加新数据
+			if (history.length > 10) history.pop(); // 设置最大历史10
+			setHistorySearch(history); // 永久存储
+			HistorySearch.value = getHistorySearch();
+		};
+		// 移除历史搜索
+		const removeHistorySearch = (isRemoveAll, id) => {
+			const history = getHistorySearch();
+			if (isRemoveAll) {
+				setHistorySearch([]);
+			} else {
+				history.forEach((e, i) => {
+					if (e.id === id) history.splice(i, 1);
+					setHistorySearch(history);
+				});
+			}
+			HistorySearch.value = getHistorySearch();
+		};
+		// 输入框回车搜索
 		const search = () => {
 			if (!keywords.value) keywords.value = keyword_default.value;
 			router.push(`/search/${keywords.value}`);
 			isShowList.value = false;
+			addNewHistorySearch(keywords.value);
 		};
-		// 用户点击关键字
+		// 用户点击关键字路由跳转搜索
 		const changKeywords = val => {
 			keywords.value = val;
 			isShowList.value = false;
+			addNewHistorySearch(keywords.value);
 		};
 		// 监视搜索框发生变化 预测用户搜索
 		watch(
@@ -124,17 +180,20 @@ export default {
 		defaultKeyword().then(data => {
 			keyword_default.value = data.data.data.showKeyword;
 		});
-		onClickOutside(target, () => (isShowList.value = false));
 		// 获取热搜列表
 		const getSearchHot = () => {
 			searchHot().then(data => {
 				data.data.data.forEach(e => {
 					e.score = useNumberSwitch(e.score);
 				});
-				searchHotList.value = data.data.data.splice(0, 10);
+				searchHotList.value = data.data.data;
 			});
 		};
-		getSearchHot();
+		onClickOutside(target, () => (isShowList.value = false));
+		onMounted(() => {
+			HistorySearch.value = getHistorySearch(); // 获取历史搜索
+			getSearchHot(); // 获取热搜列表
+		});
 		return {
 			target,
 			keyword_default,
@@ -144,6 +203,8 @@ export default {
 			search,
 			changKeywords,
 			searchHotList,
+			removeHistorySearch,
+			HistorySearch,
 		};
 	},
 };
@@ -159,11 +220,12 @@ export default {
 			left: 0.7rem;
 			height: 2rem;
 			line-height: 2rem;
+			font-size: 0.9rem;
 		}
 		input {
 			outline: none;
 			height: 2rem;
-			width: 16rem;
+			width: 18rem;
 			border: none;
 			border-radius: 1rem;
 			background-color: #e3e3e3;
@@ -172,11 +234,24 @@ export default {
 	}
 	.searchList {
 		position: absolute;
+		top: 3rem;
 		width: 100%;
 		background-color: #fff;
 		border-radius: 0.5rem;
 		z-index: 999;
-		> div {
+		filter: drop-shadow(0 0 10px rgba(0, 0, 0, 0.2));
+		&::before {
+			position: absolute;
+			top: -0.5rem;
+			left: 50%;
+			transform: translateX(-50%) rotate(45deg);
+			content: '';
+			width: 1.5rem;
+			height: 1.5rem;
+			background-color: #fff;
+			z-index: -1;
+		}
+		> div[class^='suggest'] {
 			display: flex;
 			padding: 0.5rem 1rem;
 			span {
@@ -217,34 +292,94 @@ export default {
 				}
 			}
 		}
-		.artists {
+		.suggest-artists {
 			img {
 				border-radius: 50%;
 			}
 		}
-		.albums {
+		.suggest-albums {
 			img {
 				border-radius: 0.3rem;
 			}
 		}
-		.search-hot {
-			display: block;
-			ul {
-				li {
-					a {
-						display: flex;
-						justify-content: space-between;
-						span {
-							&:nth-child(1) {
-								padding-left: 1rem;
+		.search-prompt {
+			padding: 0.5rem 1rem;
+			max-height: 37rem;
+			overflow-y: auto;
+			.hot {
+				ul {
+					span {
+						font-size: 0.9rem;
+					}
+					li {
+						margin: 0.5rem 0;
+						a {
+							display: flex;
+							align-items: center;
+							.sort {
+								width: 3rem;
+								text-align: center;
+								span {
+									font-size: 0.9rem;
+								}
 							}
-							&:nth-child(2) {
-								flex: 1;
+							.info {
+								width: calc(100% - 3rem);
+								.msg {
+									height: 1.5rem;
+									line-height: 1.5rem;
+									span {
+										font-size: 0.8rem;
+										margin-right: 0.5rem;
+									}
+									img {
+										width: 2rem;
+										height: 1rem;
+										margin-right: 0.5rem;
+									}
+								}
+								.content {
+									height: 1.5rem;
+									line-height: 1.5rem;
+									font-size: 0.7rem;
+									text-overflow: -o-ellipsis-lastline;
+									overflow: hidden;
+									text-overflow: ellipsis;
+									display: -webkit-box;
+									-webkit-line-clamp: 1;
+									-webkit-box-orient: vertical;
+								}
 							}
 						}
+						&:nth-child(-n + 4) .searchWord {
+							font-weight: bold;
+						}
+						&:nth-child(-n + 4) .sort {
+							color: #ff3a3a;
+						}
 					}
-					&:nth-child(-n + 4) > a {
-						color: #ff6a6a;
+				}
+			}
+			.history {
+				.head-history {
+					display: flex;
+					justify-content: space-between;
+					span {
+						font-size: 0.9rem;
+					}
+					i {
+						font-size: 0.9rem;
+					}
+				}
+				ul {
+					li {
+						display: flex;
+						justify-content: space-between;
+						padding-left: 1rem;
+						a,
+						i {
+							font-size: 0.8rem;
+						}
 					}
 				}
 			}
