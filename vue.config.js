3ffd7a7500070ba3bc40395f1e1d5031fe476dd9
Turmp <1737293634@qq.com>
2023-06-09 23:33:55 +0800

    🐎 ci: 关闭devServer代理

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,4 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	lintOnSave: false,
-	devServer: {
-		proxy: {
-			'/api': {
-				target: '', // Vercel/腾讯云部署后的域名
-				ws: true,
-				pathRewrite: {
-					'^/api': '', //路径重写，将接口路径中以/api开头的部分替换掉
-				},
-			},
-		},
-	},
 });
