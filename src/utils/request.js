93f118939a80bb7be9ae60451250d94f750caef2
Turmp <1737293634@qq.com>
2023-06-09 23:42:31 +0800

    🐞 fix: 修复接口缓存和cookie携带问题
    修复时间戳在params内无效的问题，修复项目部署后cookie无法携带的问题（在请求拦截器中将所有get请求转为post请求并在请求体中传入cookie）

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,8 +4,8 @@ import message from '@/utils/message';
 // 创建axios实例
 const instance = axios.create({
 	// 基础路径
-	// baseURL: '/api',
-	baseURL: 'http://localhost:3000/',
+	baseURL: 'https://service-nsjbrcma-1317235276.gz.apigw.tencentcs.com/release/',
+	// baseURL: 'http://localhost:3000/',
 	/*
 	  *请求API为本地时将baseURL路径替换为http://localhost:3000/
 		!Vercel/腾讯云部署时请配置代理
@@ -18,6 +18,15 @@ const instance = axios.create({
 // 请求拦截器
 instance.interceptors.request.use(
 	config => {
+		config.data = {
+			cookie: document.cookie,
+		};
+		config.method = 'post';
+		if (config.url.indexOf('?') === -1) {
+			config.url = config.url + '?timestamp=' + Date.now();
+		} else {
+			config.url = config.url + '&timestamp=' + Date.now();
+		}
 		return config;
 	},
 	error => {
@@ -32,9 +41,13 @@ instance.interceptors.response.use(
 		return response;
 	},
 	error => {
-		console.log(error);
 		if (error.response.status === 400) {
-			message({ type: 'error', message: '当前网络拥挤，请稍后再试！' });
+			const code = error.response.data.code;
+			if (code === -462) {
+				message({ type: 'error', message: error.response.data.data.blockText });
+			} else if (code === -460) {
+				message({ type: 'error', message: error.response.data.message });
+			}
 		}
 		return Promise.reject(error);
 	}
